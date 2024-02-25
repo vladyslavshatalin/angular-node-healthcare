8396bec362f8d8f76ffcdf0593cf0fa2aa8be6ef
“vladyslavshatalin” <vladyslavshatalin@gmail.com>
2024-02-25 21:23:50 +0100

    Implement Authguard

--- angular/src/app/services/auth-guard.service.ts
+++ angular/src/app/services/auth-guard.service.ts
@@ -10,10 +10,34 @@ export class AuthGuardService implements CanActivate {
   constructor(public dataService: DataService, public router: Router) { }
 
   canActivate(): boolean {
-    // return true if authenticated else redirect to login page
+    /* let isAuthenticated = false;
+    try {
+      this.dataService.getAuthStatus().map((data) => {
+        isAuthenticated = data
 
-    return;
+      })
+    } catch (e) {
+      isAuthenticated = false
+    } finally {
+      if (!isAuthenticated) {
+        this.router.navigate(['/login'])
+      }
+      return isAuthenticated;
+    } */
+    let isAuthenticated: boolean = false;
+
+    this.dataService.getAuthStatus().subscribe((status: boolean) => {
+      isAuthenticated = status;
+    }, (error: any) => {
+      console.error('Error checking authentication:', error);
+      isAuthenticated = false;
+    });
+    if (!isAuthenticated) {
+      this.router.navigate(['/login']); // Redirect to error page in case of an error
+    }
+    return isAuthenticated;
   }
+
 
 }
 
