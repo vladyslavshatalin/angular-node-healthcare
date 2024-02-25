dd9b8b0609e4229bcdf2ad9bbc37ef68e89a70a7
“vladyslavshatalin” <vladyslavshatalin@gmail.com>
2024-02-25 22:12:15 +0100

    Implement header component

--- angular/src/app/component/header/header.component.ts
+++ angular/src/app/component/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, DoCheck, Inject } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import {DataService } from '../../services/data.service';
-import {ActivatedRoute} from '@angular/router';
+import { DataService } from '../../services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,26 +11,37 @@ import {ActivatedRoute} from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   userDetails: any = {};
-  
-  constructor(private dataService: DataService) { 
+
+  constructor(private dataService: DataService) {
 
   }
 
   ngOnInit() {
 
     // call getProfileDetails method to get user details
+    this.getProfileDetails()
 
   }
 
   getProfileDetails() {
 
-  // call getUserDetails method of dataService and assign response to userDetails property
+    // call getUserDetails method of dataService and assign response to userDetails property
+    this.dataService.getUserDetails().subscribe(
+      (response) => {
+        // Successful login
+        this.userDetails = response;
+      },
+      (error) => {
+        // Handle login error (display error message, etc.)
+      }
+    );
 
   }
 
   logout() {
 
     // call doLogOut method
-    
+    this.dataService.doLogOut()
+
   }
 }
