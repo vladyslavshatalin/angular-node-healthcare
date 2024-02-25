700ee5c3641c3b03cb9474b5bd035d822316d8ca
“vladyslavshatalin” <vladyslavshatalin@gmail.com>
2024-02-25 22:26:36 +0100

    Implement middleware and getProfile

--- nodejs/API/getProfile.js
+++ nodejs/API/getProfile.js
@@ -1,5 +1,5 @@
-  let user=require('../models/user');
-let mongoose=require('mongoose');
+let user = require('../models/user');
+let mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('../config');
@@ -7,8 +7,25 @@ const mongo = require('mongodb').MongoClient
 
 
 
-module.exports=function(req, res) {
-    
-    /*write your code here*/
+module.exports = function (req, res) {
 
-  }
+  /*write your code here*/
+  user.findOne({ _id: req.query.uid }, (error, attemptedUser) => {
+    console.log(attemptedUser, "attemptedUser")
+    /* if (!attemptedUser || req.body.pwd !== attemptedUser.pwd) {
+       return res.status(400).json({ error: 'Username or password is wrong' });
+    } */
+
+    // Generate JWT token
+    const loggedInUser = {
+      uid: attemptedUser._id,
+      userName: attemptedUser.userName,
+      email: attemptedUser.email,
+      location: attemptedUser.location,
+      mobile: attemptedUser.mobile
+    };
+    console.log("loggedInUser", loggedInUser);
+    res.json(loggedInUser);
+  })
+
+}
--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -3,9 +3,42 @@ const config = require('./config.js');
 
 let checkToken = (req, res, next) => {
 
-    /*write your middleware code here*/
-    
-	};
+  /*write your middleware code here*/
+  const secretToken = config.secret; // Secret token for generating JWT
+
+  // Extract the token from the request headers
+  const authHeader = req.header('Authorization') || req.header('x-access-token');
+  /* if (!authHeader) {
+    console.log('token not valid')
+    return res.status(400).json({ error: 'Please authenticate' });
+  } */
+
+  const token = authHeader; // Extract the token part
+  console.log("authHeader", authHeader)
+  console.log("token", token)
+  console.log("secretToken", secretToken)
+
+  /* if (!token) {
+    console.log('token not found')
+    return res.status(400).json({ error: 'Please authenticate' });
+  } */
+
+  // try {
+  // Verify the token using the secret key
+  const decoded = jwt.verify(token, secretToken);
+  // if (decoded._id.length > 0) {
+  console.log('token verified')
+  next();
+  /* } else {
+    console.log('token invalid', decoded)
+    return res.status(400).json({ error: 'Please authenticate' });
+  } */
+  /* } catch (error) {
+    console.log('middleware error', error)
+    return res.status(400).json({ error: 'Please authenticate' });
+  } */
+
+};
 
 
 module.exports = {
