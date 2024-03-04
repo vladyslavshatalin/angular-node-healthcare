dddade76479da014e1d3161da52fef025464d3fe
“vladyslavshatalin” <vladyslavshatalin@gmail.com>
2024-03-04 21:49:34 +0100

    Implement get all appointments

--- angular/src/app/component/all-requested-appointments/all-requested-appointments.component.ts
+++ angular/src/app/component/all-requested-appointments/all-requested-appointments.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 import * as alertify from 'alertify.js';
 
 @Component({
   selector: 'app-all-requested-appointments',
   templateUrl: './all-requested-appointments.component.html',
-  styleUrls: ['./all-requested-appointments.component.css']
+  styleUrls: ['./all-requested-appointments.component.css'],
+  providers: [DatePipe]
 })
 export class AllRequestedAppointmentsComponent implements OnInit {
 
@@ -17,17 +19,29 @@ export class AllRequestedAppointmentsComponent implements OnInit {
 
   ngOnInit() {
     // call appointments method by default
+    this.appointments();
   }
 
   appointments() {
-
     // get all requested appointments from service
+    this.dataService.requestedAppointments().subscribe(
+      (response) => {
+        // Successful login
+        this.allAppointments = response;
+      },
+      (error) => {
+        // Handle login error (display error message, etc.)
+      }
+    );
+
+    console.log(this.allAppointments)
 
   }
 
   view(patientId) {
 
     // should navigate to 'patientList' page with selected patientId
+    this.route.navigate(['/patientList', patientId])
 
   }
 
@@ -36,7 +50,15 @@ export class AllRequestedAppointmentsComponent implements OnInit {
     // delete selected appointment uing service
 
     // After deleting the appointment, get all requested appointments
-
+    this.dataService.deleteAppointment(id).subscribe(
+      (response) => {
+        // Successful login
+        this.appointments()
+      },
+      (error) => {
+        // Handle login error (display error message, etc.)
+      }
+    );
 
   }
 
--- nodejs/API/deleteAppointment.js
+++ nodejs/API/deleteAppointment.js
@@ -1,11 +1,19 @@
-let singlePatientAppointments=require('../models/appointments');
-let mongoose=require('mongoose');
+let singlePatientAppointments = require('../models/appointments');
+let mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('../config');
 const mongo = require('mongodb').MongoClient
 
-module.exports=function(req,res){
-    
-    /*write your code here*/
-  }
+module.exports = function (req, res) {
+
+  /*write your code here*/
+
+  singlePatientAppointments.findOneAndRemove({ _id: req.query.appointmentId }, function (error, createdData) {
+    if (error) {
+      return res.status(400).json({ error: err });
+    }
+    return res.status(200).json({ status: "success" });
+  })
+
+}
